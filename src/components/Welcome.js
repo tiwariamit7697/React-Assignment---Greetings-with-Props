2d81093588f74629ed3aa84f3e68158be6ca27a7
Amit <amitkumartiwari527@gmaial.com>
2020-11-11 18:44:46 +0530

    commit

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,9 @@ import React from "react";
 export default function Welcome(props)
 {
     return (
+    <div>
     <h1>Hey {props.name}!</h1>
     <h2>Welcome to Newton School.</h2>
+    </div>
     )
 }
